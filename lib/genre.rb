fabf865d37f98d70ce162cd01bfe341ba3e33af8
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-11-19 19:21:32 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
+class Genre
+
+attr_accessor :name, :songs, :artists, :type
+
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def songs
+    Song.all.select { |song| song.genre == self}
+end
+
+def artists
+    songs.map do |song|
+        song.artist
+    end
+end
+
+def self.all
+    @@all
+end
+
+end
 
